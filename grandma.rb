11c4036f7a9f8427eb7ac605b71280de31d708ec
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-03-27 20:08:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,14 @@
+def speak_to_grandma(phrase)
+ if phrase == "I love you grandma!"
+ puts "I LOVE YOU TOO PUMPKIN!"
+elsif phrase.upcase !=phrase
+ puts "HUH?! SPEAK UP SONNY!!"
+else 
+ puts "NO, NOT SINCE 1938!"
+end
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
