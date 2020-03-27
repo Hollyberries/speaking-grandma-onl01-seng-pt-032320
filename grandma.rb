3ea45e0ea2e9395d5f9a9799fd86659040b1e832
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-03-27 20:12:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
  if phrase == "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN!"
+  puts "I LOVE YOU TOO PUMPKIN!"
  elsif phrase.upcase !=phrase
-  return "HUH?! SPEAK UP, SONNY!"
+  puts "HUH?! SPEAK UP, SONNY!"
  else 
-  return "NO, NOT SINCE 1938!"
+  puts "NO, NOT SINCE 1938!"
  end
 
 end
